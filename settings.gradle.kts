bcab2856823377490a718b93c027d5bca2e94fb2
wcaleniekuba <126691749+wcaleniekuba@users.noreply.github.com>
2023-03-23 17:58:16 +0100

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        maven("https://maven.fabricmc.net/")
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-rootProject.name = "BasicAgent"
